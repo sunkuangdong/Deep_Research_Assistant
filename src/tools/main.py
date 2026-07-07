6c08947d52f758641db40fe40baf471d409458a8
sunkuangdong <769296817@qq.com>
2026-07-06 19:53:57 -0700

    add the eidtor to confirm that should I need to analyze

--- src/tools/main.py
+++ src/tools/main.py
@@ -1,8 +1,10 @@
 import asyncio
-from src.tools.analyst_agent import run_research_analyze_review
+# from src.tools.analyst_agent import run_research_analyze_review
+from src.tools.editor import run_workflow
 
 async def main():
-    out = await run_research_analyze_review("AI Agent 框架对比")
+    # out = await run_researcher_once("AI Agent 框架对比")
+    out = await run_workflow("AI Agent 框架对比")
     print(out)
 
 if __name__ == "__main__":
